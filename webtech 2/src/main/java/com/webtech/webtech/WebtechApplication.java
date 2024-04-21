51173bcf7c32ddd50533a1fee83cd9d18ccc5650
Jana Pham <phamjana7@gmail.com>
2024-04-22 00:05:33 +0200

    1.commit

--- webtech 2/src/main/java/com/webtech/webtech/WebtechApplication.java
+++ webtech 2/src/main/java/com/webtech/webtech/WebtechApplication.java
@@ -2,11 +2,6 @@ package com.webtech.webtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.CommandLineRunner;
-import java.util.Arrays;
-
 
 
 
@@ -16,20 +11,5 @@ public class WebtechApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebtechApplication.class, args);
 	}
-
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Hallo, hier kannst du To-Dos erstellen.");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			for (String beanName : beanNames) {
-				System.out.println(beanName);
-			}
-
-		};
-	}
-
 
 }
